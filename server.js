03a820de703e94d59308fedf408c2229fc36ca71
KaanHGunay <kaangunay16@gmail.com>
2019-03-10 17:08:06 +0300

    testing

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users) });
+app.get('/', (req, res) => { res.send('it is working') });
 app.post('/signin', signin.handleSignin(db, bcrypt));
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
@@ -31,5 +31,5 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on 3000 port`);
+    console.log(`app is running on ${process.env.PORT} port`);
 });
