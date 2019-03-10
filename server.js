588f6b0335d369364e7c02c4b68f46dad8d5465a
KaanHGunay <kaangunay16@gmail.com>
2019-03-10 18:04:31 +0300

    data url base added

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host : 'postgresql-elliptical-66212',
-        user: 'postgres',
-        password: '9632',
-        database: 'smartbrain'
+        host : process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
